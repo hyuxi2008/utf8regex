6f55e594df15d1bea70c9c32989a6d193e6a1cd8
Jakub Wilk <jwilk@jwilk.net>
2010-09-18 13:30:46 +0200

    Update my e-mail address.

--- utf8re-locale.h
+++ utf8re-locale.h
@@ -1,5 +1,5 @@
 /**
- * Copyright © 2009 Jakub Wilk <ubanus@users.sf.net>
+ * Copyright © 2009 Jakub Wilk <jwilk@jwilk.net>
  *
  * This library is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License version
